f071aff91c1ec68fb21569fd07aee567cb3d968b
yjs2952 <yjs2952@naver.com>
2020-07-12 23:42:27 +0900

    개인 정보 수정 1

---
+++ src/main/java/next/web/HomeServlet.java
@@ -0,0 +1,27 @@
+package next.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/")
+public class HomeServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+    private static final Logger log = LoggerFactory.getLogger(UpdateUserFormServlet.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        log.debug("index");
+
+        RequestDispatcher rd = req.getRequestDispatcher("/index.jsp");
+        rd.forward(req, resp);
+    }
+}
---
+++ src/main/java/next/web/LoginUserServlet.java
@@ -0,0 +1,34 @@
+package next.web;
+
+import core.db.DataBase;
+import next.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/user/login")
+public class LoginUserServlet extends HttpServlet {
+
+    private static final Logger log = LoggerFactory.getLogger(UpdateUserFormServlet.class);
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        User user = DataBase.findUserById(req.getParameter("userId"));
+        log.debug("user : {}", user);
+
+        if (user == null) {
+            resp.sendRedirect("/user/list");
+        }
+
+        HttpSession httpSession = req.getSession();
+        httpSession.setAttribute("user", user);
+        resp.sendRedirect("/");
+    }
+}
