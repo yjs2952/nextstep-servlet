924ad9e45f6035e65303452fdf2c146a244487b1
yjs2952 <yjs2952@naver.com>
2020-07-15 14:48:47 +0900

    최초 커밋

---
+++ src/main/java/next/controller/Controller.java
@@ -0,0 +1,9 @@
+package next.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface Controller {
+
+    String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception;
+}
